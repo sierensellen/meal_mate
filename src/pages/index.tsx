7b888af9e025afb57371f960141cf0c2e5ccec69
Bavo Vanderghote <bavo.vanderghote+github@gmail.com>
2022-03-02 20:12:27 +0100

    feat: convert to TS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
-import Head from 'next/head'
-import clientPromise from '../lib/mongodb'
+import { HomeProps } from '@shared/types/Home.types';
+import {NextPage} from 'next';
+import Head from 'next/head';
+// import clientPromise from '../../lib/mongodb';
 
-export default function Home({ isConnected }) {
+const Home: NextPage<HomeProps> = ({ isConnected }) => {
   return (
     <div className="container">
       <Head>
@@ -222,21 +224,23 @@ export default function Home({ isConnected }) {
   )
 }
 
-export async function getServerSideProps(context) {
-  try {
-    // client.db() will be the default database passed in the MONGODB_URI
-    // You can change the database by calling the client.db() function and specifying a database like:
-    // const db = client.db("myDatabase");
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-    await clientPromise
-    return {
-      props: { isConnected: true },
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      props: { isConnected: false },
-    }
-  }
-}
+// export async function getServerSideProps(context) {
+  // try {
+  //   // client.db() will be the default database passed in the MONGODB_URI
+  //   // You can change the database by calling the client.db() function and specifying a database like:
+  //   // const db = client.db("myDatabase");
+  //   // Then you can execute queries against your database like so:
+  //   // db.find({}) or any of the MongoDB Node Driver commands
+  //   await clientPromise
+  //   return {
+  //     props: { isConnected: true },
+  //   }
+  // } catch (e) {
+  //   console.error(e)
+  //   return {
+  //     props: { isConnected: false },
+  //   }
+  // }
+// }
+
+ export default Home;
